09fcccca623f48ec222826c6ba34a7f591eb3600
Supphakit Duanghoy <nakaharaml9@gmail.com>
2026-07-30 05:32:29 +0700

    src/App.jsx: mentioned magic number

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ export const App = () => {
                         Tested Eaglercrafts HTML files:<br></br>
                         - EaglercraftX_1.8_u53_WASM-GC_Offline.html (expected assets.epw sha256: ecfa438804724de30a871408058b25504746ab14705486cd9ec32855676cc524)<br></br>
                         - Eaglercraft_1.12.2_u3_WASM_Offline.html (expected assets.epw sha256: 6dc65357ddc681ee95ec8623850de6389d746c723fb7329eb3f8746ffcbbafb3)<br></br>
-                        Valid assets.epw must have a file header of "|EAG$WASM|", hexdump output example below.<br></br>
+                        Valid assets.epw file must have a magic number of "45 41 47 24 57 41 53 4D" ("|EAG$WASM|"), hexdump output example below.<br></br>
                         <br></br>
                         $ hexdump -n 8 -C assets.epw<br></br>
                         00000000  45 41 47 24 57 41 53 4d                           |EAG$WASM|
